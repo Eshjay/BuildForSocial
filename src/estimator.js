c2c255e94674459d87de03e3cf132bac28ff0c1f
eshjay <olushola.abiodun@ovi123>
2020-04-08 02:37:08 +0100

    restrucure my data

--- src/estimator.js
+++ src/estimator.js
@@ -1,20 +1,19 @@
-   const covid19ImpactEstimator = (data) =>{
-    let data = Object.create(Object.prototype,
-        {
-            region: {
-            name: "Africa",
-            avgAge: 19.7,
-            avgDailyIncomeInUSD: 5,
-            avgDailyIncomePopulation: 0.71
-            },
-            periodType: "days",
-            timeToElapse: 58,
-            reportedCases: 674,
-            population: 66622705,
-            totalHospitalBeds: 1380614
-           }
-        );
-    return();
+let inputData =[
+    {
+    region: {
+    name: "Africa",
+    avgAge: 19.7,
+    avgDailyIncomeInUSD: 5,
+    avgDailyIncomePopulation: 0.71
+    },
+    periodType: "days",
+    timeToElapse: 58,
+    reportedCases: 674,
+    population: 66622705,
+    totalHospitalBeds: 1380614
+   };
+];
+const covid19ImpactEstimator = (data) =>{
 };
 
 export default covid19ImpactEstimator;
